7406b8cad6d27ba6ae7372e3c4796634989b83bf
Christina Leung <cgleungsf@gmail.com>
2020-06-03 23:07:17 -0700

    heroku deploy attempt

--- models/Workout.js
+++ models/Workout.js
@@ -1,4 +1,6 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema(
   {
@@ -50,6 +52,6 @@ WorkoutSchema.virtual("totalDuration").get(function () {
   }, 0);
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
-module.exports = model('Workout', WorkoutSchema);
+module.exports = Workout;
